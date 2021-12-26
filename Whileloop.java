4de1220120e8039d0b8806e61317bf809e2de5a8
NaveenJoshuvaDev <njworkholic@gmail.com>
2021-12-26 22:35:51 +0530

    Done

---
+++ Whileloop.java
@@ -0,0 +1,17 @@
+public class Whileloop
+{
+public static void main(String[] args)
+{
+int no=1;
+while(no<=20)
+{
+if((no%3==0)&&(no%5==0))//condition needed to be checked
+{
+System.out.println(no);
+}
+
+}
+no++;
+}
+}
+
