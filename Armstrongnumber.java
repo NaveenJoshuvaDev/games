b9039a31ddcab67763b09f0f491740811605af63
NaveenJoshuvaDev <njworkholic@gmail.com>
2021-12-27 09:04:33 +0530

    done

---
+++ Armstrongnumber.java
@@ -0,0 +1,26 @@
+package tamilnadu.chennai;
+
+public class Armstrongnumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		 int number, n, total = 0;
+	      System.out.println("Armstrong number between 1 to 1000: ");
+	      for(int a = 1; a <= 1000; a++)
+	      {
+	         number = a;
+	         while(number > 0)
+	         {
+	            n = number % 10;
+	            total = total + (n * n * n);
+	            number = number / 10;
+	         }
+	         if(total == a)
+	         {
+	            System.out.print(a + " ");
+	         }
+	         total = 0;
+	      }
+	   }
+	}
